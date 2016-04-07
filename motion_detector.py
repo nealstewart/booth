9497ae5ee662599228c7c87e248ce2b9e9765c70
Neal Stewart <nes@tradeshift.com>
2016-04-07 21:32:09 +0200

    Ghosting effect

--- motion_detector.py
+++ motion_detector.py
@@ -4,70 +4,53 @@ import datetime
 import imutils
 import time
 import cv2
+import numpy as np
+from collections import deque
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
-args = vars(ap.parse_args())
+AREA = 1000
+QUEUE_SIZE = 10
 
-firstFrame = None
 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
+def doCameraStuff():
     camera = cv2.VideoCapture(0)
     time.sleep(0.25)
 
-# otherwise, we are reading from a video file
-else:
-    camera = cv2.VideoCapture(args["video"])
+    myqueue = deque(list(), QUEUE_SIZE)
 
-    # initialize the first frame in the video stream
-    firstFrame = None
+    lastFrame = None
+    while True:
+        (grabbed, frame) = camera.read()
+        text = "Unoccupied"
 
+        if not grabbed:
+            break
 
-count = 0
-GHOST_INTERVAL = 10
+        frame = imutils.resize(frame, width=AREA)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
+        if lastFrame is None:
+            lastFrame = gray
+            continue
 
-# loop over the frames of the video
-while True:
-    count += 1
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    (grabbed, frame) = camera.read()
-    text = "Unoccupied"
+        frameDelta = cv2.absdiff(lastFrame, gray)
+        lastFrame = gray
 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if not grabbed:
-        break
+        myqueue.append(frameDelta)
 
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=700)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        s = np.zeros(lastFrame.shape, dtype=np.uint8)
 
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
+        for idx, delta in enumerate(myqueue):
+            s = cv2.add(s, delta)
 
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
+        cv2.imshow("Frame Delta", s)
 
-    cv2.imshow("Frame Delta", frameDelta)
+        key = cv2.waitKey(1) & 0xFF
 
-    if count % GHOST_INTERVAL == 0:
-        firstFrame = gray
+        if key == ord("q"):
+            break
 
-    key = cv2.waitKey(1) & 0xFF
+    camera.release()
+    cv2.destroyAllWindows()
 
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
-
-# cleanup the camera and close any open windows
-camera.release()
-cv2.destroyAllWindows()
+doCameraStuff()
