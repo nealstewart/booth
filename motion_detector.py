20875218fcaacac60b1db0dd6b1bc9f58b68c66b
Neal Stewart <nes@tradeshift.com>
2016-04-07 20:25:24 +0200

    Working on the motion detector

---
+++ motion_detector.py
@@ -0,0 +1,73 @@
+# import the necessary packages
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
+
+firstFrame = None
+
+# if the video argument is None, then we are reading from webcam
+if args.get("video", None) is None:
+    camera = cv2.VideoCapture(0)
+    time.sleep(0.25)
+
+# otherwise, we are reading from a video file
+else:
+    camera = cv2.VideoCapture(args["video"])
+
+    # initialize the first frame in the video stream
+    firstFrame = None
+
+
+count = 0
+GHOST_INTERVAL = 10
+
+
+# loop over the frames of the video
+while True:
+    count += 1
+    # grab the current frame and initialize the occupied/unoccupied
+    # text
+    (grabbed, frame) = camera.read()
+    text = "Unoccupied"
+
+    # if the frame could not be grabbed, then we have reached the end
+    # of the video
+    if not grabbed:
+        break
+
+    # resize the frame, convert it to grayscale, and blur it
+    frame = imutils.resize(frame, width=700)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # if the first frame is None, initialize it
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+
+    # compute the absolute difference between the current frame and
+    # first frame
+    frameDelta = cv2.absdiff(firstFrame, gray)
+
+    cv2.imshow("Frame Delta", frameDelta)
+
+    if count % GHOST_INTERVAL == 0:
+        firstFrame = gray
+
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
+
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
